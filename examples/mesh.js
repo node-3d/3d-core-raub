038b44f307a140352c45b06be6168311c2d8786d
Luis Blanco <raubtierxxx@gmail.com>
2017-08-23 23:04:18 +0300

    mesh nvidia ok

--- examples/mesh.js
+++ examples/mesh.js
@@ -1,21 +1,21 @@
 'use strict';
 
-const node3d  = require('../index');
+const node3d = require('../index');
+const THREE  = node3d.three;
 
-
-const camera = new node3d.three.PerspectiveCamera( 75, node3d.canvas.width / node3d.canvas.height, 1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, node3d.canvas.width / node3d.canvas.height, 1, 1000 );
 camera.position.z = 500;
 
-const scene = new node3d.three.Scene();
+const scene = new THREE.Scene();
 
-const geometry = new node3d.three.IcosahedronGeometry(200, 1);
-const material =  new node3d.three.MeshLambertMaterial({
-	color: 0xffffff,
+const geometry = new THREE.IcosahedronGeometry(200, 1);
+const material =  new THREE.MeshLambertMaterial({
+	color: 0xFFFFFF,
 });
-const mesh = new node3d.three.Mesh(geometry, material);
+const mesh = new THREE.Mesh(geometry, material);
 scene.add( mesh );
 
-const pointLight = new node3d.three.PointLight(0xFFFFFF, 1, 100000);
+const pointLight = new THREE.PointLight(0xFFFFFF, 1, 100000);
 scene.add( pointLight );
 pointLight.position.x = 200;
 pointLight.position.y = 2000;
@@ -24,8 +24,9 @@ pointLight.position.z = 500;
 
 function animation() {
 	
-	mesh.rotation.x = Date.now() * 0.00005;
-	mesh.rotation.y = Date.now() * 0.0001;
+	mesh.rotation.x = Date.now() * 0.0005;
+	mesh.rotation.y = Date.now() * 0.001;
+	mesh.rotation.z = Date.now() * 0.0007;
 	
 	node3d.renderer.render(scene, camera);
 	
