aa966668d3abc2ca1a9d7b49db113b640bc2ffaf
Luis Blanco <luisblanco1337@gmail.com>
2021-05-09 10:22:47 +0300

    Debug tests 2

--- test/index.test.js
+++ test/index.test.js
@@ -4,159 +4,159 @@ const init = require('..');
 
 
 const inited = init();
-const {
-	gl, three, Document, Window, Image,
-	Brush, Cloud, Drawable, Lines, Points, Rect, Screen, Surface, Tris,
-} = inited;
+// const {
+// 	gl, // three, Document, Window, Image,
+// 	Brush, Cloud, Drawable, Lines, Points, Rect, Screen, Surface, Tris,
+// } = inited;
 
 
-const staticClasses = {
+// const staticClasses = {
 	
-	Brush: {
-		create({ screen }) {
-			return new Brush({ screen });
-		},
-		props: ['size', 'pos', 'visible', 'color'],
-		methods: [],
-	},
+// 	Brush: {
+// 		create({ screen }) {
+// 			return new Brush({ screen });
+// 		},
+// 		props: ['size', 'pos', 'visible', 'color'],
+// 		methods: [],
+// 	},
 	
-	Cloud: {
-		create({ screen }) {
+// 	Cloud: {
+// 		create({ screen }) {
 			
-			const vertices = [];
-			for (let i = 30; i > 0; i--) {
-				vertices.push( Math.random() * 2000 - 1000 );
-			}
-			const pos = gl.createBuffer();
-			gl.bindBuffer(gl.ARRAY_BUFFER, pos);
-			gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+// 			const vertices = [];
+// 			for (let i = 30; i > 0; i--) {
+// 				vertices.push( Math.random() * 2000 - 1000 );
+// 			}
+// 			const pos = gl.createBuffer();
+// 			gl.bindBuffer(gl.ARRAY_BUFFER, pos);
+// 			gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 			
-			return new Cloud({ screen, count: 10, attrs: { position: { vbo: pos, items: 3 } } });
-		},
-		props: ['three', 'screen', 'mat', 'geo', 'mesh', 'visible'],
-		methods: [],
-	},
+// 			return new Cloud({ screen, count: 10, attrs: { position: { vbo: pos, items: 3 } } });
+// 		},
+// 		props: ['three', 'screen', 'mat', 'geo', 'mesh', 'visible'],
+// 		methods: [],
+// 	},
 	
-	Drawable: {
-		create({ screen }) {
-			return new Drawable({ screen });
-		},
-		props: ['three', 'screen', 'mat', 'geo', 'mesh', 'z', 'visible', 'pos', 'color'],
-		methods: [],
-	},
+// 	Drawable: {
+// 		create({ screen }) {
+// 			return new Drawable({ screen });
+// 		},
+// 		props: ['three', 'screen', 'mat', 'geo', 'mesh', 'z', 'visible', 'pos', 'color'],
+// 		methods: [],
+// 	},
 	
-	Points: {
-		create({ screen }) {
+// 	Points: {
+// 		create({ screen }) {
 			
-			const vertices = [];
-			for (let i = 30; i > 0; i--) {
-				vertices.push( Math.random() * 2000 - 1000 );
-			}
-			const pos = gl.createBuffer();
-			gl.bindBuffer(gl.ARRAY_BUFFER, pos);
-			gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+// 			const vertices = [];
+// 			for (let i = 30; i > 0; i--) {
+// 				vertices.push( Math.random() * 2000 - 1000 );
+// 			}
+// 			const pos = gl.createBuffer();
+// 			gl.bindBuffer(gl.ARRAY_BUFFER, pos);
+// 			gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 			
-			return new Points({ screen, count: 10, attrs: { position: { vbo: pos, items: 3 } } });
-		},
-		props: ['three', 'screen', 'mat', 'geo', 'mesh', 'visible'],
-		methods: [],
-	},
+// 			return new Points({ screen, count: 10, attrs: { position: { vbo: pos, items: 3 } } });
+// 		},
+// 		props: ['three', 'screen', 'mat', 'geo', 'mesh', 'visible'],
+// 		methods: [],
+// 	},
 	
-	Lines: {
-		create({ screen }) {
+// 	Lines: {
+// 		create({ screen }) {
 			
-			const vertices = [];
-			for (let i = 30; i > 0; i--) {
-				vertices.push( Math.random() * 2000 - 1000 );
-			}
-			const pos = gl.createBuffer();
-			gl.bindBuffer(gl.ARRAY_BUFFER, pos);
-			gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+// 			const vertices = [];
+// 			for (let i = 30; i > 0; i--) {
+// 				vertices.push( Math.random() * 2000 - 1000 );
+// 			}
+// 			const pos = gl.createBuffer();
+// 			gl.bindBuffer(gl.ARRAY_BUFFER, pos);
+// 			gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 			
-			return new Lines({ screen, count: 10, attrs: { position: { vbo: pos, items: 3 } } });
-		},
-		props: ['three', 'screen', 'mat', 'geo', 'mesh', 'visible'],
-		methods: [],
-	},
+// 			return new Lines({ screen, count: 10, attrs: { position: { vbo: pos, items: 3 } } });
+// 		},
+// 		props: ['three', 'screen', 'mat', 'geo', 'mesh', 'visible'],
+// 		methods: [],
+// 	},
 	
-	Tris: {
-		create({ screen }) {
+// 	Tris: {
+// 		create({ screen }) {
 			
-			const vertices = [];
-			for (let i = 30; i > 0; i--) {
-				vertices.push( Math.random() * 2000 - 1000 );
-			}
-			const pos = gl.createBuffer();
-			gl.bindBuffer(gl.ARRAY_BUFFER, pos);
-			gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+// 			const vertices = [];
+// 			for (let i = 30; i > 0; i--) {
+// 				vertices.push( Math.random() * 2000 - 1000 );
+// 			}
+// 			const pos = gl.createBuffer();
+// 			gl.bindBuffer(gl.ARRAY_BUFFER, pos);
+// 			gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 			
-			return new Tris({ screen, count: 10, attrs: { position: { vbo: pos, items: 3 } } });
-		},
-		props: ['three', 'screen', 'mat', 'geo', 'mesh', 'visible'],
-		methods: [],
-	},
+// 			return new Tris({ screen, count: 10, attrs: { position: { vbo: pos, items: 3 } } });
+// 		},
+// 		props: ['three', 'screen', 'mat', 'geo', 'mesh', 'visible'],
+// 		methods: [],
+// 	},
 	
-	Rect: {
-		create({ screen }) {
-			return new Rect({ screen });
-		},
-		props: ['three', 'screen', 'mat', 'geo', 'mesh', 'visible'],
-		methods: [],
-	},
+// 	Rect: {
+// 		create({ screen }) {
+// 			return new Rect({ screen });
+// 		},
+// 		props: ['three', 'screen', 'mat', 'geo', 'mesh', 'visible'],
+// 		methods: [],
+// 	},
 	
-	Screen: {
-		create() {
-			return new Screen();
-		},
-		props: [
-			'three', 'canvas', 'camera', 'scene', 'renderer', 'context',
-			'document', 'width', 'height', 'w', 'h', 'size', 'title', 'fov',
-		],
-		methods: [],
-	},
+// 	Screen: {
+// 		create() {
+// 			return new Screen();
+// 		},
+// 		props: [
+// 			'three', 'canvas', 'camera', 'scene', 'renderer', 'context',
+// 			'document', 'width', 'height', 'w', 'h', 'size', 'title', 'fov',
+// 		],
+// 		methods: [],
+// 	},
 	
-	Surface: {
-		create({ screen }) {
-			return new Surface({ screen });
-		},
-		props: [
-			'canvas', 'camera', 'scene', 'renderer', 'context',
-			'document', 'title', 'fov', 'size', 'texture'
-		],
-		methods: [],
-	},
+// 	Surface: {
+// 		create({ screen }) {
+// 			return new Surface({ screen });
+// 		},
+// 		props: [
+// 			'canvas', 'camera', 'scene', 'renderer', 'context',
+// 			'document', 'title', 'fov', 'size', 'texture'
+// 		],
+// 		methods: [],
+// 	},
 	
 
-};
+// };
 
 
-const initedClasses = {
+// const initedClasses = {
 	
-	Image: {
-		create() {
-			return new Image();
-		},
-		props: ['src'],
-		methods: ['on'],
-	},
+// 	Image: {
+// 		create() {
+// 			return new Image();
+// 		},
+// 		props: ['src'],
+// 		methods: ['on'],
+// 	},
 	
-	Window: {
-		create() {
-			return new Window();
-		},
-		props: ['size'],
-		methods: ['show'],
-	},
+// 	Window: {
+// 		create() {
+// 			return new Window();
+// 		},
+// 		props: ['size'],
+// 		methods: ['show'],
+// 	},
 	
-	Document: {
-		create() {
-			return new Document();
-		},
-		props: ['body'],
-		methods: ['createElement'],
-	},
+// 	Document: {
+// 		create() {
+// 			return new Document();
+// 		},
+// 		props: ['body'],
+// 		methods: ['createElement'],
+// 	},
 	
-};
+// };
 
 
 describe('Node.js 3D Core', () => {
