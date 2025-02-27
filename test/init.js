eb3ca491eb9528a210bb41969448e5278a3b1ea4
Luis Blanco <luisblanco1337@gmail.com>
2025-02-27 15:45:46 +0400

    Try stencil bits

--- test/init.js
+++ test/init.js
@@ -1,5 +1,6 @@
 const { platform } = require('node:process');
 const three = require('three');
+const glfw = require('glfw-raub');
 
 const { init, addThreeHelpers } = require('..');
 
@@ -12,6 +13,10 @@ const initOptsMac = {
 	isGles3: false,
 	isWebGL2: false,
 };
+
+if (platform === 'darwin') {
+	glfw.windowHint(glfw.STENCIL_BITS, 8);
+}
 
 const inited = init(platform === 'darwin' ? initOptsMac : initOpts);
 addThreeHelpers(three, inited.gl);
