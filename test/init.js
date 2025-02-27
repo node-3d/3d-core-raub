1c7e3a25bd38b2dd94cb5b5754eca396b324f1e5
Luis Blanco <luisblanco1337@gmail.com>
2025-02-27 15:39:34 +0400

    Adjust test

--- test/init.js
+++ test/init.js
@@ -1,11 +1,19 @@
+const { platform } = require('node:process');
 const three = require('three');
 
 const { init, addThreeHelpers } = require('..');
 
-const inited = init({
+const initOpts = {
 	isGles3: true,
 	isWebGL2: true,
-});
+};
+const initOptsMac = {
+	...initOpts,
+	isGles3: false,
+	isWebGL2: false,
+};
+
+const inited = init(platform === 'darwin' ? initOptsMac : initOpts);
 addThreeHelpers(three, inited.gl);
 
 module.exports = inited;
