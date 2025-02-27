c90fe399df918d320fd5904836d3b8db24d67be5
Luis Blanco <luisblanco1337@gmail.com>
2025-02-27 16:15:41 +0400

    Adjust test

--- test/init.js
+++ test/init.js
@@ -12,10 +12,14 @@ const initOptsMac = {
 	...initOpts,
 	isGles3: false,
 	isWebGL2: false,
+	major: 2,
+	minor: 1,
 };
 
 if (platform === 'darwin') {
 	glfw.windowHint(glfw.STENCIL_BITS, 8);
+	// this would be nice... - https://github.com/glfw/glfw/pull/2571
+	// glfw.windowHint(glfw.CONTEXT_RENDERER, glfw.SOFTWARE_RENDERER);
 }
 
 const inited = init(platform === 'darwin' ? initOptsMac : initOpts);
